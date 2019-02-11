2b9bc464b8ff23d103a5522ec94ab10065f47eaa
Diaa Abujaber <diaa1999abujaber@gmail.com>
2019-02-11 15:15:35 +0300

    documented runner.py

--- src/runner.py
+++ src/runner.py
@@ -5,14 +5,22 @@ import os
 TYPE_ERROR_MESSAGE = ':expected python type file\nEnter __name__ -h for more information'
 
 def raise_error(error):
-	print(f'error:{error}')
+	print(f'error:{error}') #format error message
 
 def checkType(file):
+	""" to ensure the passed file type isn't
+		anything other than python type. Otherwise, 
+		it stops the programme from executing. """
+
 	if(not file[-3:] == '.py'):
 		raise_error(TYPE_ERROR_MESSAGE)
 		sys.exit()
 
 def check_path(path):
+	""" checks if the passed file path is
+		valid. Otherwise, it stops the programme 
+		from executing. """
+
 	if(path != os.getcwd()):
 		try:
 			os.chdir(path)
@@ -21,17 +29,29 @@ def check_path(path):
 			sys.exit()
 
 def check_code(file):
+	""" to see if the passed python file is
+	    executing properly. Otherwise, it stops 
+	    the programme. """
+
 	if(os.system(f'python -m py_compile {file}')): #returns true when the the code has error
 		sys.exit()
 
 def parse():
+	""" returns the entered file path and file name
+		from the command line """
+
 	parser = argparse.ArgumentParser()
 	parser.add_argument('FILENAME', type = str, 
-						help = 'python file name')
+						help = 'python file name') #essential argument
+
+	""" optional argument if the file
+	 	is in the current directory (default 
+	 	value equals current directory)"""
+
 	parser.add_argument('-P', '--filepath', type = str, 
 						help = 'path to the file directory.\
 						Default is the current directory', 
-						default = os.getcwd())
+						default = os.getcwd()) 
 	
 	args = parser.parse_args()
 
@@ -41,10 +61,11 @@ def run():
 
 	(file_path, file_name) = parse()
 	
+	""" required steps before proceeding into the code """
 	checkType(file_name)
 	check_path(file_path)
 	check_code(file_name)
 
 
 if __name__ == '__main__':
-	sys.exit()
+	sys.exit() 
