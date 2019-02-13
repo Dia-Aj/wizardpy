4f6b56dfd05a2cdb1074cce90f250c30fa5d6e71
Diaa Abujaber <diaa1999abujaber@gmail.com>
2019-02-13 18:34:51 +0300

    Document expression modular

--- src/analyzer/expression.py
+++ src/analyzer/expression.py
@@ -1,15 +1,31 @@
+
 from dataclasses import dataclass
 import sys
 
 @dataclass
 class exp:
-	name : str
-	type : str
-	description : str
-	pattern : repr
+	'''to encapsulate each regex details
+	   which would make it easier to track
+	   errors'''
+
+	#regex name
+	name : str 
+	
+	#regex catagory
+	type : str 
+
+	'''an example code (as string) 
+	   for the corresponding regex'''
+	description : str   
+
+	#the regex patteren
+	pattern : repr 
 
 	def __init__(self, name, type, description, pattern):
 		self.name = name
 		self.type = type
 		self.description = description
 		self.pattern = pattern
+
+if __name__ == '__main__':
+	sys.exit()
