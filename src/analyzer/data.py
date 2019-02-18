783e642a810fa9e3097c59262bdc6bd04a763136
Diaa Abujaber <diaa1999abujaber@gmail.com>
2019-02-18 18:34:08 +0300

    Add new syntax Regex for testing purpose

--- src/analyzer/data.py
+++ src/analyzer/data.py
@@ -1,7 +1,16 @@
 regex = {
-	'regex_name': [
-		'sometype',
-		r'\n(\w+\s*)=(\s*[\da-zA-z]+)',
-		'some_description',
+	'space-sep_defining': [
+		'syntax',
+		r'(\w+\s*=\s*\w+\n*)+',
+		'''
+		this regex find variables defined on the following form:-
+			x = 10
+			y = 20
+			z = 3
+		And fix it to
+			x, y, z = 10, 20, 3
+
+		this works only on consecutive definitions.
+		''',
 	],
 }
