d920cde771e526adce490bd7511bad0c7f8b2a05
Diaa Abujaber <diaa1999abujaber@gmail.com>
2019-02-25 15:54:31 +0300

    Implement testing code for regexes

--- src/analyzer/testing/test.py
+++ src/analyzer/testing/test.py
@@ -8,3 +8,28 @@ sys.path.append(
 
 from analyzer.analyzer import Regex
 
+passed_code = '''foo = 10
+fun = 20
+bar = 30
+baz = 40'''
+
+expected_code = '''foo, fun, bar, baz = 10, 20, 30, 40'''
+
+
+class testRegex(unittest.TestCase):
+	def test_spacesep_defining(self):
+		Regex.check_regex(passed_code)
+
+		with open('optimizedCode.py', 'r') as file:
+			output = file.read()
+			
+		self.assertIn(expected_code, output)
+
+
+if __name__ == '__main__':
+	unittest.main()
+
+
+
+
+
