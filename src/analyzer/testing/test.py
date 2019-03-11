8b28f74444f669b890e368c7e3d3aa79957bbabf
Diaa Abujaber <diaa1999abujaber@gmail.com>
2019-03-11 13:11:30 +0300

    Fix bugs

--- src/analyzer/testing/test.py
+++ src/analyzer/testing/test.py
@@ -6,7 +6,6 @@ path = os.path.split(sys.argv[0])[0]
 sys.path.append(
 			'\\'.join(path.split('\\')[:-2]))
 
-
 from analyzer.analyzer import Regex
 
 class testRegex(unittest.TestCase):
@@ -20,7 +19,8 @@ class testRegex(unittest.TestCase):
 			try:
 				self.assertIn(test.expected_code, output)
 			except AssertionError:
-				print(test.expected_code)
+				print(f'error expected:\n{test.expected_code}'\
+					f'\nbut found:\n{output}')
 
 if __name__ == '__main__':
 	unittest.main()
