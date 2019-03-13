e7920ddade8098cf746ff2db0b886f710a38406d
Diaa Abujaber <diaa1999abujaber@gmail.com>
2019-03-13 13:48:08 +0300

    Implement fix_chained_comparison() code

--- src/optimizer/optimizer.py
+++ src/optimizer/optimizer.py
@@ -9,6 +9,10 @@ functions = {}
 def log_function(function):
 	functions.setdefault(function.__name__, function)
 
+def flip(op):
+	flipped_ops = {'>':'<', '<': '>', '>=' : '<=', '<=' : '>='}
+	return flipped_ops[op]
+
 class code_optimizer:
 	"""takes the passed harmfull code and returns
 	   the idiomatic code"""
@@ -20,10 +24,13 @@ class code_optimizer:
 
 	@log_function
 	def fix_spacesep_defining(self, matches, code):
-		harmful = [match.group(0) for match in matches]
-		groups = [a.split() for a in harmful]
-
-		for group in groups:
+		harmful = [
+			(match.group(0), match.span())
+			for match in matches
+		]
+		groups = [a[0].split() for a in harmful]
+		
+		for i, group in enumerate(groups):
 			string, numeric = group[0], group[2]
 			for i, j in enumerate(group):
 				if(i < 3 or j == '='): continue
@@ -32,8 +39,47 @@ class code_optimizer:
 				else:
 					string += f', {j}'
 			
-			fix = f'{string} = {numeric}\n\n'
-			code = re.sub(harmful[groups.index(group)], fix, code)
+			fix = f'{string} = {numeric}\n'
+
+			ind = groups.index(group)
+			if(ind < len(harmful)-1 and 
+						harmful[ind][1][1] != harmful[ind+1][1][0]):
+				fix+='\n'
+			
+			code = re.sub(harmful[ind][0], fix, code)
+
+		return code
+
+	@log_function
+	def fix_chained_comparison(self, matches, code):
+
+		for match in matches:
+
+			op1, op2 = match.group('OP1'), match.group('OP2')
+			if(match.group(1) == match.group(4)):
+				(left, mid, right) = (match.group(3), 
+											match.group(1), match.group(6))
+				op1 = flip(op1)
+
+			elif(match.group(1) == match.group(6)):
+				(left, mid, right) = (match.group(3), 
+											match.group(1), match.group(4))
+				(op1, op2) = (flip(op1), flip(op2))
+
+			elif(match.group(3) == match.group(4)):
+				(left, mid, right) = (match.group(1), 
+											match.group(3), match.group(6))
+
+			elif(match.group(3) == match.group(6)):
+				(left, mid, right) = (match.group(1), 
+											match.group(3), match.group(4))
+				op2 = flip(op2)
+			
+			else:
+				continue
+				
+			fix = f'{left} {op1} {mid} {op2} {right}'
+			code = re.sub(match.group(0), fix, code)
 
 		return code
 
