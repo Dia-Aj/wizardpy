0e23724c463e0829c14d99e2b3fa9f327e6d97a0
Diaa Abujaber <diaa1999abujaber@gmail.com>
2019-03-13 13:45:54 +0300

    Create new chained_comparison regex

--- src/analyzer/data.py
+++ src/analyzer/data.py
@@ -2,15 +2,25 @@ regex = {
 	'spacesep_defining': [
 		'syntax',
 		r'(\w+\s*=\s*\w+\n*){2,5}',
+
 		'''
-		this regex find variables defined on the following form:-
+		E.g.
 			x = 10
 			y = 20
 			z = 3
-		And fix it to
+		fixed it to
 			x, y, z = 10, 20, 3
-
-		this works only on consecutive definitions.
-		''',
+		this works only on consecutive definitions.''',
 	],
+
+	'chained_comparison': [
+		'syntax',
+		r'(\w+)\s*(?P<OP1>[<=>]+)\s*(\w+)\s*and\s*(\w)+\s*(?P<OP2>[<=>]+)\s*(\w)+',
+		'''
+		E.g.
+			if(y >= z and x<=z)
+		fixed to
+			if(y >= z >= x) '''
+	],
+
 }
