7e1b09e5179dda27b1330c1a0e3d7903cf8f9c3b
Diaa Abujaber <Diaa1999abujaber@gmail.com>
2019-03-25 11:28:24 +0200

    Add new regex to the dictionary
    The new regular expression fixes the repeation of the variable name
    over and over for the '==' operation.

--- src/analyzer/data.py
+++ src/analyzer/data.py
@@ -4,11 +4,10 @@ regex = {
 		r'(\w+\s*=\s*\w+\n*){2,5}',
 
 		'''
-		E.g.
 			x = 10
 			y = 20
 			z = 3
-		fixed it to
+		fix to
 			x, y, z = 10, 20, 3
 		this works only on consecutive definitions.''',
 	],
@@ -17,10 +16,21 @@ regex = {
 		'syntax',
 		r'(\w+)\s*(?P<OP1>[<=>]+)\s*(\w+)\s*and\s*(\w)+\s*(?P<OP2>[<=>]+)\s*(\w)+',
 		'''
-		E.g.
-			if(y >= z and x<=z)
-		fixed to
-			if(y >= z >= x) '''
+			if(y >= z and x<=z):
+		fix to
+			if(y >= z >= x): '''
 	],
+
+	'repeated_variable_or_comparsion': [
+		'conditional',
+		r'(\w+\s*==\s*[\'\"]?[A-Za-z0-9\.]+[\'\"]?\s*(or)?\s*)+',
+		'''
+			if(x == 1 or x == 2 or x == 3):
+		fix to
+			if(x in (1,2,3)):
+
+		'''
+	],
+	
 
 }
